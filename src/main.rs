4eb7fc2c352e535de8238c06587f22913799d782
Elinvynia <59487684+Elinvynia@users.noreply.github.com>
2021-04-20 15:54:13 +0200

    Format

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
-use std::sync::Arc;
+use nanoid::nanoid;
 use std::fs::{read_to_string, write};
+use std::sync::Arc;
+use tera::{Context, Tera};
+use tide::http::{mime, Url};
 use tide::prelude::Deserialize;
 use tide::{Redirect, Request, Response};
-use tide::http::{mime, Url};
-use tera::{Tera, Context};
-use nanoid::nanoid;
 
 const LOCATION: &str = "127.0.0.1:8000";
 const ALPHABET: [char; 55] = [
-    '1', '2', '3', '4', '5', '6', '7', '8', '9',
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j',
-    'k', 'm', 'n', 'p', 'q', 'r', 's', 't', 'u',
-    'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
-    'E', 'F', 'G', 'H', 'J', 'K', 'M', 'N', 'P',
-    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
-    'Z'
+    '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'k', 'm', 'n', 'p', 'q',
+    'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'M', 'N', 'P', 'Q',
+    'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
 ];
 
 #[async_std::main]
@@ -24,10 +20,8 @@ async fn main() {
 
     let mut app = tide::with_state(state);
 
-
-    app.at("/").get(|req: Request<State>| async move {
-        Ok(req.state().render("index.tera"))
-    });
+    app.at("/")
+        .get(|req: Request<State>| async move { Ok(req.state().render("index.tera")) });
 
     app.at("create").post(|mut req: Request<State>| async move {
         let urlform: UrlForm = req.body_form().await?;
@@ -37,50 +31,42 @@ async fn main() {
         context.insert("shortcut", &format!("{}/{}", LOCATION, id));
         Ok(req.state().render_with_context("create.tera", &context))
     });
-    
+
     app.at("*").get(|req: Request<State>| async move {
         let url = read_to_string(format!("shortcuts/{}", &req.url().path()[1..]))?;
         Ok(Redirect::new(url))
     });
-
 
     app.listen(LOCATION).await.unwrap();
 }
 
 #[derive(Debug, Clone, Deserialize)]
 struct UrlForm {
-  url: Url,
+    url: Url,
 }
 
 #[derive(Debug, Clone)]
 struct State {
-    tera: Arc<Tera>
+    tera: Arc<Tera>,
 }
 
 impl State {
     fn tera(&self) -> &Tera {
         self.tera.as_ref()
     }
+
     fn render(&self, name: &str) -> Response {
         let render = self
             .tera()
             .render(name, &Context::new())
             .unwrap_or_else(|err| err.to_string());
 
-        Response::builder(200)
-            .content_type(mime::HTML)
-            .body(render)
-            .build()
+        Response::builder(200).content_type(mime::HTML).body(render).build()
     }
-    fn render_with_context(&self, name: &str, context: &Context) -> Response {
-        let render = self
-            .tera()
-            .render(name, context)
-            .unwrap_or_else(|err| err.to_string());
 
-        Response::builder(200)
-            .content_type(mime::HTML)
-            .body(render)
-            .build()
+    fn render_with_context(&self, name: &str, context: &Context) -> Response {
+        let render = self.tera().render(name, context).unwrap_or_else(|err| err.to_string());
+
+        Response::builder(200).content_type(mime::HTML).body(render).build()
     }
 }
